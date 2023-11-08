dbe9ddfa97308c83f4b6ad9a50a04b20dd1a3693
hojinchoi99 <hojinch99@gmail.com>
2023-11-08 18:23:17 +0900

    #3.2 Create Account

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,5 +1,8 @@
 import { styled } from "styled-components";
 import { useState } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
     height: 100%;
@@ -42,6 +45,7 @@ const Error = styled.span`
 `
 
 export default function CreateAccount() {
+    const navigate = useNavigate();
     const [isLoading, setLoading] = useState(false);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -57,19 +61,30 @@ export default function CreateAccount() {
             setPassword(value)
         }
     }
-    const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    const onSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        if(isLoading || name === "" || email === "" || password === "") return;
         try {
-            
+            setLoading(true);
+            const credentials = await createUserWithEmailAndPassword(
+                auth, 
+                email, 
+                password
+                );
+            console.log(credentials.user);
+            await updateProfile(credentials.user, {
+                displayName: name,
+            });
+            navigate("/");
         } catch (e) {
-
+            setError("0");
         } finally {
             setLoading(false);
         }
     }
     return (
         <Wrapper>
-            <Title>Log into X</Title>
+            <Title>Join X</Title>
             <Form onSubmit={onSubmit}>
                 <Input
                     onChange={onChange}
