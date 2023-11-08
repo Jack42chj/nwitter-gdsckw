798c6aeaf602afad4f165f0ef67e8bb47ad5886f
hojinchoi99 <hojinch99@gmail.com>
2023-11-08 20:55:53 +0900

    #3.4 Log In

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -5,7 +5,7 @@ export default function ProtectedRoute({
     children } : { children : React.ReactNode }) {
         const user = auth.currentUser;
         if(!user){
-            return <Navigate to="/create-account" />;
+            return <Navigate to="/login" />;
         }
         return children;
     }
